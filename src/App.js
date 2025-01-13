60ccae7a5de0bb3470cb79f6bd53f83fb131452f
SamuelLoiselle <48161393+SamuelLoiselle@users.noreply.github.com>
2025-01-13 15:55:09 -0600

    initial commit, react app, gitignore etc

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to ClickSmart Website!</h1>
+      <p>Your React app is ready to go!</p>
+    </div>
+  );
+}
+
+export default App;
